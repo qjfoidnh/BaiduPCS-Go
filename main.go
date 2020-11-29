4781bd7ca970d6abcbd06a99e7c5f901b35d28ee
qjfoidnh <qjfoidnh@126.com>
2020-11-29 22:59:23 +0800

    添加本地文件输出通用秒传链接

--- main.go
+++ main.go
@@ -1351,7 +1351,7 @@ func main() {
 						[]string{"前256KB切片的md5", strSliceMd5},
 						[]string{"crc32", strCrc32},
 						[]string{"秒传命令", app.Name + " rapidupload -length=" + strLength + " -md5=" + strMd5 + " -slicemd5=" + strSliceMd5 + " -crc32=" + strCrc32 + " " + fileName},
-						[]string{"通用秒传链接", strMd5 + "#" + strSliceMd5 + "#" + strLength + regFileName},
+						[]string{"通用秒传链接", strMd5 + "#" + strSliceMd5 + "#" + strLength + "#" + regFileName},
 					})
 					tb.Render()
 					fmt.Printf("\n")
