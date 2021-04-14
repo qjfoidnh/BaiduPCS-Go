41583f86c6340ed65af68399a70e759028d95d82
qjfoidnh <qjfoidnh@126.com>
2021-04-14 17:25:36 +0800

    v3.7.9

--- main.go
+++ main.go
@@ -55,7 +55,7 @@ const (
 
 var (
 	// Version 版本号
-	Version = "v3.7.8-devel"
+	Version = "v3.7.9-devel"
 
 	historyFilePath = filepath.Join(pcsconfig.GetConfigDir(), "pcs_command_history.txt")
 	reloadFn        = func(c *cli.Context) error {
