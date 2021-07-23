4834ec0859e797f23f378bd23cc0dbc1217e8751
qjfoidnh <qjfoidnh@126.com>
2021-07-23 13:28:57 +0800

    Update build.sh
    add darwin osx arm support

--- build.sh
+++ build.sh
@@ -104,6 +104,7 @@ touch ./vendor/golang.org/x/sys/windows/windows.s
 
 # OS X / macOS
 Build $name-$version"-darwin-osx-amd64" darwin amd64
+Build $name-$version"-darwin-osx-arm64" darwin arm64
 # Build $name-$version"-darwin-osx-386" darwin 386
 
 # Windows
