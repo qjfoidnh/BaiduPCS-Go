9e7fc9c679abfc0b21b19df97413d52da51292b2
zhujixx <79738310+zhujixx@users.noreply.github.com>
2022-08-24 01:19:08 +0800

    Tree with size option

--- internal/pcscommand/tree.go
+++ internal/pcscommand/tree.go
@@ -2,6 +2,7 @@ package pcscommand
 
 import (
 	"fmt"
+        "strconv"	
 	"github.com/qjfoidnh/BaiduPCS-Go/baidupcs"
 	"strings"
 )
@@ -15,9 +16,36 @@ const (
 type (
 	TreeOptions struct {
 		Depth    int
+		ShowSize bool
 		ShowFsid bool
 	}
 )
+
+func Format(n int64) string {
+    in := strconv.FormatInt(n, 10)
+    numOfDigits := len(in)
+    if n < 0 {
+        numOfDigits-- // First character is the - sign (not a digit)
+    }
+    numOfCommas := (numOfDigits - 1) / 3
+
+    out := make([]byte, len(in)+numOfCommas)
+    if n < 0 {
+        in, out[0] = in[1:], '-'
+    }
+
+    for i, j, k := len(in)-1, len(out)-1, 0; ; i, j = i-1, j-1 {
+        out[j] = in[i]
+        if i == 0 {
+            return string(out)
+        }
+        if k++; k == 3 {
+            j, k = j-1, 0
+            out[j] = ','
+        }
+    }
+}
+
 
 func getTree(pcspath string, depth int, option *TreeOptions) {
 	var (
@@ -56,14 +84,18 @@ func getTree(pcspath string, depth int, option *TreeOptions) {
 			continue
 		}
 
-		if i+1 == fN {
-			prefix = lastFilePrefix
-		}
-		if option.ShowFsid {
-			fmt.Printf("%v%v %v: %v\n", indentPrefixStr, prefix, file.Filename, file.FsID)
-		} else {
-			fmt.Printf("%v%v %v\n", indentPrefixStr, prefix, file.Filename)
-		}
+	    if i+1 == fN {
+	      prefix = lastFilePrefix
+	    }
+	    if option.ShowFsid && option.ShowSize {
+	      fmt.Printf("%v%v %v: %v: %v\n", indentPrefixStr, prefix, file.Filename, Format(file.Size), file.FsID)
+	    } else if option.ShowFsid {
+	      fmt.Printf("%v%v %v: %v\n", indentPrefixStr, prefix, file.Filename, file.FsID)
+	    } else if option.ShowSize {
+	      fmt.Printf("%v%v %v: %v\n", indentPrefixStr, prefix, file.Filename, Format(file.Size))
+	    } else {
+	      fmt.Printf("%v%v %v\n", indentPrefixStr, prefix, file.Filename)
+	    }
 	}
 
 	return
