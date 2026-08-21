a15b2aa6c6f969f60a731d7c5cf403c296d54d10
qjfoidnh <qjfoidnh@126.com>
2026-08-21 16:39:23 +0800

    Add missing import for 'sync' package

--- internal/pcsfunctions/pcsupload/upload.go
+++ internal/pcsfunctions/pcsupload/upload.go
@@ -5,11 +5,13 @@ import (
 	"github.com/qjfoidnh/BaiduPCS-Go/baidupcs"
 	"github.com/qjfoidnh/BaiduPCS-Go/baidupcs/pcserror"
 	"github.com/qjfoidnh/BaiduPCS-Go/internal/pcsconfig"
+	"github.com/qjfoidnh/BaiduPCS-Go/requester"
 	"github.com/qjfoidnh/BaiduPCS-Go/requester/multipartreader"
 	"github.com/qjfoidnh/BaiduPCS-Go/requester/rio"
 	"github.com/qjfoidnh/BaiduPCS-Go/requester/uploader"
 	"io"
 	"net/http"
+	"sync"
 	"time"
 )
 
